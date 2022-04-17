cc893a2d66e671fee71b664c7e2370fce9cd7a92
althafdaa <althaf.akbar@sejutacita.id>
2022-04-17 13:18:33 +0700

    Feat: finished swagger challenge

---
+++ backend/server/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    username: 'postgres',
+    password: 'root',
+    database: 'binar-swagger',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+  test: {
+    username: 'postgres',
+    password: 'root',
+    database: 'binar-swagger',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+  production: {
+    username: 'postgres',
+    password: 'root',
+    database: 'binar-swagger',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+};
